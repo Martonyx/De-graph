2242287ad3147ccf9121cb8f5d36ecf55933b520
MARTIN <Martonyx>
2022-09-28 06:55:24 +0100

    whitelist dapp contract

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+const { ethers } = require("hardhat")
+
+async function main() {
+  const whitelistContract = await ethers.getContractFactory("whitelist")
+
+  const deployedWhitelistContract = await whitelistContract.deploy(10)
+
+  await deployedWhitelistContract.deployed()
+
+  console.log("Whitelist Contract Address:", deployedWhitelistContract.address)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
